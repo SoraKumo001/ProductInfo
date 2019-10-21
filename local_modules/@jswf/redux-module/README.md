d0df908b0226a67991e0982ecd857853bcc91bf4
SoraKumo <info@croud.jp>
2019-10-21 14:29:35 +0900

    no message

---
+++ local_modules/@jswf/redux-module/README.md
@@ -0,0 +1,273 @@
+# @jswf/redux-module
+
+[![npm version](https://badge.fury.io/js/%40jswf%2Fredux-module.svg)](https://badge.fury.io/js/%40jswf%2Fredux-module)
+
+Make Redux operations modular
+
+- Document  
+[https://ttis.croud.jp/?uuid=ed418ee1-730b-40d7-b748-f2969d8d430d](https://ttis.croud.jp/?uuid=ed418ee1-730b-40d7-b748-f2969d8d430d)
+
+## １．Usage
+
+### 1.1 createStore
+
+Associate ModuleReducer with Store
+
+```tsx
+//Store create
+const store = createStore(ModuleReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root") as HTMLElement
+);
+```
+
+### 1.2 Creating a store module class
+
+When using TypeScript, the structure of module data can be specified
+defaultState can be omitted, but in that case undefined is returned when data is accepted.
+
+```tsx
+//Store module data type
+export interface State {
+  msg: string;
+  count: number;
+}
+export class TestModule extends ReduxModule<State> {
+  //init value
+  static defaultState: State = {
+    msg: "init",
+    count: 0,
+  };
+}
+```
+
+### 1.3 Reading and writing data with the Hooks component
+
+```tsx
+function HooksApp() {
+  const module = useModule(TestModule);
+  const value = module.getState()!;
+  return (
+    <div
+      style={{ border: "solid 1px", display: "inline-block", padding: "1em" }}
+    >
+      <div>AppComponent</div>
+      <button
+        onClick={() => {
+          module.setState({ msg: "click!", count: value.count + 1 });
+        }}
+      >
+        button
+      </button>
+      <div>{value.msg}</div>
+      <div>{value.count}</div>
+    </div>
+  );
+}
+```
+
+### 1.4 Reading and writing data with the Class component
+
+```tsx
+class _ClassApp extends Component {
+  render() {
+    const module = mapModule(this.props, TestModule);
+    const value = module.getState()!;
+    return (
+      <div
+        style={{ border: "solid 1px", display: "inline-block", padding: "1em" }}
+      >
+        <div>AppComponent</div>
+        <button
+          onClick={() => {
+            //Set data
+            module.setState({ msg: "click!", count: value.count + 1 });
+          }}
+        >
+          button
+        </button>
+        <div>{value.msg}</div>
+        <div>{value.count}</div>
+      </div>
+    );
+  }
+}
+const ClassApp = mapConnect(_ClassApp, TestModule);
+```
+
+### 1.5 Reference another class
+
+```ts
+export interface TestState2 {
+  text: string;
+}
+export class TestModule2 extends ReduxModule<TestState2> {
+  static includes = [TestModule]; //Declare the external class used here
+  static defaultState: TestState2 = {
+    text:"A"
+  };
+  getMessage() {
+    //Call external class with getModule
+    return this.getModule(TestModule).getState("msg");
+  }
+  setMessage(msg: string) {
+    this.getModule(TestModule).setState({ msg });
+  }
+  getCount() {
+    return this.getModule(TestModule).getState("count")!;
+  }
+  setCount(count: number) {
+    this.getModule(TestModule).setState({ count });
+  }
+  addText() {
+    return this.setState(this.getText()+"a","text")!;
+  }
+  getText() {
+    return this.getState("text")!;
+  }
+}
+```
+
+### 1.6 Write only attribute
+
+ If you use the ReduxModule inheritance class in the component without specifying it, side effects will occur whenever the data under class management is updated.
+
+ If it is used only for writing, useless processing will occur. In that case, it can be avoided by attaching the writeOnly attribute.
+
+- For Function components (Hooks)  
+Set the third argument of useModule to true
+
+```ts
+const testModule = useModule(TestModule,undefined,true);
+```
+
+- For Class component  
+Set the writeOnly attribute to true at the time of mapConnect
+
+```ts
+const ClassApp = mapConnect(_ClassApp, [{module:TestModule,writeOnly:true}]);
+```
+
+- For external reference  
+
+```ts
+static includes = [{module:TestModule,writeOnly:true}];
+```
+
+### 1.7 Sample
+
+```tsx
+import React, { Component } from "react";
+import * as ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import {
+  ModuleReducer,
+  useModule,
+  ReduxModule,
+  mapModule,
+  mapConnect
+} from "@jswf/redux-module";
+
+/**
+ *Data structure definition (when using TypeScript)
+ *
+ * @export
+ * @interface TestState
+ */
+export interface TestState {
+  msg: string;
+}
+/**
+ * Store access class
+ * (Automatically allocate space in the store for each class)
+ * @export
+ * @class TestModule
+ * @extends {ReduxModule<TestState>}
+ */
+export class TestModule extends ReduxModule<TestState> {
+  //Initial value can be set here
+  protected static defaultState: TestState = {
+    msg: "初期値"
+  };
+  // It is not always necessary to create the following access methods
+  // getState and setState are public so you can rewrite directly from the outside
+  public getMessage() {
+    return this.getState("msg")!;
+  }
+  public setMessage(msg: string) {
+    this.setState({ msg });
+  }
+}
+
+/**
+ *Sample for Hooks
+ *
+ * @returns
+ */
+function HooksApp() {
+  // Receive module instance
+  // The limit of the useModule can be used is the same as other hooks
+  const testModule = useModule(TestModule);
+  // The same class can have different areas by attaching a prefix as shown below
+  //const testModule = useModule(TestModule,"Prefix");
+  return (
+    <>
+      <div>FunctionComponent</div>
+      <input
+        value={testModule.getMessage()}
+        onChange={e => testModule.setMessage(e.target.value)}
+      />
+      <hr />
+    </>
+  );
+}
+
+/**
+ *Sample for Class
+ *
+ * @class _ClassApp
+ * @extends {Component}
+ */
+class _ClassApp extends Component {
+  render() {
+    // Receive module instance
+    // Note that the name and argument are slightly different from Hooks
+    const testModule = mapModule(this.props, TestModule);
+    return (
+      <>
+        <div>ClassComponent</div>
+        <input
+          value={testModule.getMessage()}
+          onChange={e => testModule.setMessage(e.target.value)}
+        />
+        <hr />
+      </>
+    );
+  }
+}
+// When using class components, map as follows
+// Only modules declared here can be used in classes
+// Multiple modules can be specified in an array
+const ClassApp = mapConnect(_ClassApp, TestModule);
+
+// Associate a dedicated reducer with Redux
+// Can be used with other reducers
+const store = createStore(ModuleReducer);
+ReactDOM.render(
+  <Provider store={store}>
+    <HooksApp />
+    <ClassApp />
+  </Provider>,
+  document.getElementById("root") as HTMLElement
+);
+
+```
+
+## ２．license
+
+MIT
---
+++ local_modules/@jswf/redux-module/src/index.ts
@@ -0,0 +1,457 @@
+import { useMemo, Component } from "react";
+import { useDispatch, useSelector, connect } from "react-redux";
+import { Dispatch } from "redux";
+
+//対象オブジェクト選択用
+type Next<U> = U extends readonly [string, ...string[]]
+  ? ((...args: U) => void) extends (top: any, ...args: infer T) => void
+    ? T
+    : never
+  : never;
+
+type Deeps<
+  State,
+  Paths extends readonly string[]
+> = Paths[0] extends keyof State
+  ? {
+      0: State[Paths[0]];
+      1: Deeps<State[Paths[0]], Next<Paths>>;
+    }[Paths[1] extends undefined ? 0 : 1]
+  : never;
+
+type map = { [key: string]: unknown };
+type moduleType<T> = {
+  new (
+    dispatch: Dispatch,
+    store: unknown,
+    prefix: string,
+    modules:
+      | undefined
+      | {
+          [key: string]: ReduxModule;
+        },
+    writeOnly: boolean
+  ): T;
+};
+
+/**
+ *Callback用アクション定義
+ *
+ * @interface Action
+ */
+interface Action {
+  type: string;
+  payload: {
+    callback: (state: map) => map;
+  };
+}
+//アクションタイプ名
+const ActionName = "@CALLBACK";
+
+/**
+ *Storeデータ設定を行う
+ *
+ * @export
+ * @template T
+ * @param {(action: Action) => void} dispatch
+ * @param {(string | string[])} name Storeのオブジェクト名(階層指定可能)
+ * @param {Partial<T>} params パラメータ
+ */
+export function setStoreState<T = map>(
+  dispatch: (action: Action) => void,
+  name: string | string[],
+  params: Partial<T>,
+  defaultTop?: unknown
+) {
+  function callback(state: map) {
+    const names = name instanceof Array ? name : [name];
+
+    const length = names.length;
+    const newState = { ...state };
+    if (defaultTop !== undefined && newState[names[0]] === undefined)
+      newState[names[0]] = defaultTop;
+
+    let tempState = newState;
+    let i;
+    for (i = 0; i < length - 1; i++) {
+      const n = names[i];
+      const state = tempState[n];
+      if (typeof state !== "object") {
+        const obj = {};
+        tempState[names[i]] = obj;
+        tempState = obj;
+      } else if (state instanceof Array) {
+        tempState[n] = [...state];
+        tempState = tempState[n] as map;
+      } else {
+        tempState[n] = { ...state };
+        tempState = tempState[n] as map;
+      }
+    }
+    if (typeof params === "object" && !(params instanceof Array))
+      tempState[names[i]] = { ...tempState[names[i]], ...params };
+    else tempState[names[i]] = params;
+    return newState;
+  }
+
+  dispatch({
+    type: ActionName,
+    payload: {
+      callback
+    }
+  });
+}
+
+/**
+ *Reduxにモジュール機能を組み込む
+ *
+ * @export
+ * @param {*} [state={}]
+ * @param {Action} action
+ * @returns
+ */
+export function ModuleReducer(state = {}, action: Action) {
+  if (action.type === ActionName)
+    return action.payload.callback(state as never);
+  return state;
+}
+
+/**
+ *ストアデータ操作用基本モジュールクラス
+ *
+ * @export
+ * @class ReduxModule
+ * @template State
+ */
+export class ReduxModule<State = { [key: string]: unknown }> {
+  protected static defaultState?: unknown;
+  public static includes?: (
+    | typeof ReduxModule
+    | { module: typeof ReduxModule; prefix?: string; writeOnly?: boolean })[];
+  private dispatch: Dispatch;
+  private store: unknown;
+  private moduleName: string;
+  private modules?: { [key: string]: ReduxModule };
+  private writeOnly: boolean;
+
+  public constructor(
+    dispatch: Dispatch,
+    store: unknown,
+    moduleName: string,
+    modules: undefined | { [key: string]: ReduxModule },
+    writeOnly: boolean
+  ) {
+    this.dispatch = dispatch;
+    this.moduleName = moduleName;
+    this.modules = modules;
+    this.writeOnly = writeOnly;
+    if (!this.writeOnly) {
+      if (store !== undefined) {
+        this.store = store;
+      } else {
+        //初期値設定
+        const defaultState = (this.constructor as Function & {
+          defaultState: State;
+        }).defaultState;
+        if (defaultState) {
+          const state = this.getState();
+          if (state === undefined) this.setState(defaultState);
+        }
+      }
+    }
+  }
+
+  /**
+   *includesに記述した外部参照モジュールを取得する
+   *
+   * @template T
+   * @template C
+   * @param {({
+   *       new (
+   *         dispatch: Dispatch,
+   *         store: unknown,
+   *         prefix: string,
+   *         modules?: { [key: string]: ReduxModule }
+   *       ): T;
+   *     } & C)} module
+   * @param {string} [prefix]
+   * @returns
+   * @memberof ReduxModule
+   */
+  getModule<T extends ReduxModule, C extends typeof ReduxModule>(
+    module: moduleType<T> & C,
+    prefix?: string
+  ) {
+    const moduleName: string = getStoreName(module, prefix);
+    return this.modules![moduleName] as T;
+  }
+  /**
+   *ReduxのDispachを取得
+   *
+   * @returns
+   * @memberof ReduxModule
+   */
+  public getDispatch() {
+    return this.dispatch;
+  }
+
+  /**
+   *モジュールのStoreデータを返す
+   *
+   * @template T
+   * @returns {(T | undefined)}
+   * @memberof StoreModule
+   */
+  public getState<K extends string[], T = State>(): T | undefined;
+  public getState<K extends string[], T = State>(
+    ...name: K
+  ): Deeps<T, K> | undefined;
+  public getState<K extends string[], T = State>(
+    ...name: K
+  ): Deeps<T, K> | T | undefined {
+    if (this.writeOnly) throw "This module is write only";
+
+    //パラメータ処理
+    let names: string[] = [];
+    if (name instanceof Array) names = name as never;
+    let store: map = this.store as map;
+    //初期値が無かった場合に仮設定
+    if (store === undefined) {
+      const defaultState = (this.constructor as Function & {
+        defaultState: State;
+      }).defaultState;
+      if (defaultState) {
+        store = defaultState as never;
+      }
+    }
+    //対象のデータを検索
+    const length = names.length;
+    if (length === 0) return (store as never) as T;
+    let i;
+    for (i = 0; i < length - 1; i++) {
+      if (store[names[i]] === undefined) return undefined;
+      store = store[names[i]] as map;
+    }
+    return store[names[i]] as Deeps<T, K>;
+  }
+
+  /**
+   *モジュールのStoreデータの設定
+   *
+   * @template T
+   * @param {(string | string[])} name
+   * @param {Partial<T>} params
+   * @memberof StoreModule
+   */
+  public setState<T = State>(params: Partial<T>): void;
+  public setState<K extends string[], T = State>(
+    params: Partial<Deeps<T, K>>,
+    ...name: K
+  ): void;
+  public setState<K extends string[], T = State>(
+    params: Partial<Deeps<T, K>>,
+    ...name: K
+  ): void {
+    const storeName = this.moduleName;
+    const names = [storeName];
+    if (typeof name === "string") names.push(name);
+    else if (name instanceof Array) names.push(...name);
+
+    const defaultState = (this.constructor as Function & {
+      defaultState: State;
+    }).defaultState;
+    setStoreState(this.dispatch, names, params as never, defaultState);
+  }
+}
+//クラス識別用マップ
+const modules = new Map<Function, number>();
+/**
+ *ストアに登録するモジュール名を生成する
+ *
+ * @export
+ * @template T
+ * @param {{
+ *     new (dispatch: Dispatch, store: unknown, prefix: string): T;
+ *   }} module
+ * @param {string} [prefix]
+ * @returns {string}
+ */
+export function getStoreName<T extends ReduxModule>(
+  module: moduleType<T>,
+  prefix?: string
+): string {
+  let count = modules.get(module);
+  if (count === undefined) {
+    count = modules.size;
+    modules.set(module, count);
+  }
+  return "@Module-" + (prefix || "") + "-" + count;
+}
+
+/**
+ *Redux操作用モジュールの取得(Hooks用)
+ *
+ * @export
+ * @template T
+ * @param  module StoreModule継承クラス
+ * @param {string} [prefix] Storeデータ分離用装飾文字列
+ * @returns {T}
+ */
+export function useModule<T extends ReduxModule, C extends typeof ReduxModule>(
+  module: moduleType<T> & C,
+  prefix?: string,
+  writeOnly?: boolean
+): T {
+  const includes = module.includes;
+  let modules: { [key: string]: ReduxModule } = {};
+  if (includes) {
+    includes.forEach(inc => {
+      if (inc instanceof Function) modules[getStoreName(inc)] = useModule(inc);
+      else
+        modules[getStoreName(inc.module, inc.prefix)] = useModule(
+          inc.module,
+          inc.prefix,
+          inc.writeOnly
+        );
+    });
+  }
+
+  const moduleName = getStoreName(module, prefix);
+  const store = writeOnly
+    ? undefined
+    : (useSelector(store => (store as map)[moduleName]) as map | undefined);
+  const dispatch = useDispatch();
+  return useMemo(() => {
+    return new module(dispatch, store, moduleName, modules, !!writeOnly);
+  }, [store, ...Object.values(modules)]);
+}
+/**
+ * モジュールを取得(Classコンポーネント用)
+ *
+ * @export
+ * @template T
+ * @param {unknown} props
+ * @param {moduleType<T>} module
+ * @param {string} [prefix]
+ * @returns
+ */
+export function mapModule<T extends ReduxModule>(
+  props: unknown,
+  module: moduleType<T>,
+  prefix?: string
+) {
+  const moduleName = getStoreName(module, prefix);
+  return (props as { modules: map }).modules[moduleName] as T;
+}
+
+/**
+ *Classコンポーネントから機能を利用するときに使用する
+ *
+ * @export
+ * @template T
+ * @template C
+ * @param {C} component
+ * @param {(moduleType<T>
+ *     | { module: moduleType<T>; prefix?: string }
+ *     | (moduleType<T> | { module: moduleType<T>; prefix?: string })[])} module
+ * @returns
+ */
+export function mapConnect<T extends ReduxModule, C extends typeof Component>(
+  component: C,
+  module:
+    | moduleType<T>
+    | { module: moduleType<T>; prefix?: string; writeOnly?: boolean }
+    | ((
+        | moduleType<any>
+        | { module: moduleType<any>; prefix?: string; writeOnly?: boolean }
+      )[])
+) {
+  const modules = module instanceof Array ? module : [module];
+
+  function mapStateToProps(state: map) {
+    const newState: map = {};
+
+    function addStoreState(
+      src:
+        | typeof ReduxModule
+        | {
+            module: typeof ReduxModule;
+            prefix?: string | undefined;
+            writeOnly?: boolean;
+          }
+    ) {
+      let module: typeof ReduxModule;
+      let prefix: string | undefined;
+      let writeOnly: boolean = false;
+      if (src instanceof Function) {
+        module = src;
+      } else {
+        module = src.module;
+        prefix = src.prefix;
+        writeOnly = !!src.writeOnly;
+      }
+      const moduleName = getStoreName(module, prefix);
+      //Store読み込みの判断
+      if (!writeOnly) newState[moduleName] = state[moduleName];
+      if (module.includes) {
+        module.includes.forEach(src => addStoreState(src));
+      }
+    }
+
+    modules.forEach(src => {
+      addStoreState(src);
+    });
+    return newState;
+  }
+
+  function mapMargeToProps(
+    store: map,
+    { dispatch }: { dispatch: Dispatch },
+    props: unknown
+  ) {
+    function addStreModule(
+      state: map,
+      src:
+        | typeof ReduxModule
+        | {
+            module: typeof ReduxModule;
+            prefix?: string | undefined;
+            writeOnly?: boolean;
+          }
+    ) {
+      let module, prefix;
+      let writeOnly: boolean = false;
+      if (src instanceof Function) {
+        module = src;
+      } else {
+        module = src.module;
+        prefix = src.prefix;
+        writeOnly = !!src.writeOnly;
+      }
+      const modules = {};
+      if (module.includes) {
+        module.includes.forEach(src => {
+          addStreModule(modules, src);
+        });
+      }
+      const moduleName = getStoreName(module, prefix);
+      const inst = new module(
+        dispatch,
+        store[moduleName],
+        moduleName,
+        modules,
+        writeOnly
+      );
+      (state as map)[moduleName] = inst;
+      return inst;
+    }
+    const state = {};
+    modules.forEach(src => addStreModule(state, src));
+    return { ...store, ...{ modules: state }, ...props };
+  }
+  return connect(
+    mapStateToProps,
+    (dispatch: Dispatch) => ({ dispatch }),
+    mapMargeToProps
+  )(component);
+}
